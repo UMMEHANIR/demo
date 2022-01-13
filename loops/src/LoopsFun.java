db062a7097e1eb979b3305ac0828a5e0c7728445
hani <uhanir005@gmail.com>
2022-01-13 22:53:30 +0530

    k

---
+++ loops/src/LoopsFun.java
@@ -0,0 +1,68 @@
+
+
+	import java.util.Scanner;
+
+	public class LoopsFun {
+
+		public static void main(String[] args) {
+		
+			
+//			for(int index = 1; index < 51; index++) {
+//				System.out.println("Java seems exciting...");
+//			}
+////			
+//			for(int index = 1; index < 15; index++) {
+//				if(index == 11)
+//					break;
+////					continue;
+//				System.out.println(index);
+//			}
+//			//init
+//			while(condition) {
+//				//inc/dec
+//			}
+			
+//			int value = 50;
+//			
+//			while(value >= 1) {
+//				System.out.println(value);
+//				value--;
+//			}
+			
+			
+//			For printing the reverse if a number taken from runtime
+//			System.out.println("Enter number:");
+//		int rev = new Scanner(System.in).nextInt();
+//		int other = 0;
+		
+//		for(;rev > 0;) {
+//			other = rev % 10;
+//			rev /= 10;
+//			System.out.print(other);
+//		}
+		
+//		Or through the while loop
+//			int vr=98;
+//		while(vr < 100) {
+////	
+//			System.out.println(vr);
+//			vr++;
+//		}
+			
+			int vr = 10;
+			
+			do {
+				System.out.println(vr);
+				vr++;
+			}while(vr < 100);
+		
+		
+			
+			
+			
+			
+
+		}
+
+	}
+
